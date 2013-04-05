4c0bab15d243a58664558dfacf28beea86383080
Matt Dennebaum <matt@ninjacipher.com>
2013-04-05 15:34:14 -0400

    working on testing

--- test.js
+++ test.js
@@ -1,5 +1,41 @@
-var Client = require('./lib/client');
+var Client = require('./lib/client'),
+Request = require('./lib/request'),
+argv = require('optimist').argv;
+
+var client = new Client();
+
+if(argv.m === 'firehose'){
+  client.connect(function(err){
+    if(err){
+      console.error(err);
+    }else{
+      console.log('Client connected');
+      req = new Request('/firehose', 'GET');
+      req.setTxnAcceptMulti();
+      client.doApiCall(
+        req,
+        function(err, response){
+          console.log(response);
+        }
+      );
+    }
+  });
+}else{
+  client.connect(function(err){
+    if(err){
+      console.error(err);
+    }else{
+      console.log('Client connected');
+      req = new Request('/ping', 'GET');
+      client.doApiCall(
+        req,
+        function(err, response){
+          console.log(response);
+        }
+      );
+    }
+  });
+}
 
 
-var client = new Client({});
-client.connect();
+
